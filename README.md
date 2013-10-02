81b52e6eb4843af56a21d48b005d77bea05d970f
Bhaskar Mookerji <mookerji@gmail.com>
2013-10-01 18:15:14 -0700

    Source tree instantiation. OS X additions to .gitignore.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-pl2
-===
+# pl2 - RTree test implementation
 
-rtree
+Pure Python implementation of an RTree, for geospatial indexing. 
+
+More explanation will appear here as needed (in the meantime, checkout
+[R-tree](http://en.wikipedia.org/wiki/R-tree)).
+
+# Package Requirements? 
+
+# Installation Instructions
+
+# Usage Examples
+
+# Questions? 
+
+Email <mookerji@gmail.com>
+
+# License 
+
+Copyright (C) 2013 Bhaskar Mookerji. Distributed under the MIT License.  
---
+++ python/setup.py
@@ -0,0 +1,17 @@
+ #! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'rtree',
+      version = '0.0.1',
+      description = 'Python RTree implementation',
+      author = 'Bhaskar Mookerji',
+      author_email = 'mookerji@gmail.com',
+      url = 'https://github.com/mookerji/pl2',
+      packages = ['rtree', 
+                  'rtree.rtree', 
+                  'rtree.tests.test_bench',
+                  'rtree.tests.test_rtree'],
+      classifiers = ["Development Status :: 2 - Pre-Alpha",
+                     "Environment :: Console"]
+)
